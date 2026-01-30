dcb50a22f7050fde89e49459a201d3c3dac1112b
Alexander <ageofinoswe@gmail.com>
2026-01-30 15:37:28 -0800

    added general purpose registers and set/get

--- Chip.cpp
+++ Chip.cpp
@@ -1,7 +1,7 @@
 #include "Chip.h"
 
 Chip::Chip(std::string fileName)
-    : programCounter{0x200}
+    : programCounter{0x200}, generalRegisters{}
 {   
     int programStart = programCounter;
 
@@ -44,4 +44,25 @@ uint16_t Chip::fetch()
     uint16_t lsb = ram.readMem(programCounter + 0x1);
     incrementProgramCounter();
     return msb | lsb;
+}
+
+uint8_t Chip::getRegisterValue(const int registerNumber) const
+{
+    if(registerNumber >= 0 && (registerNumber < sizeof(generalRegisters) / sizeof(generalRegisters[0])))
+    {
+        return generalRegisters[registerNumber];
+    }
+    
+    return -1;
+}
+
+uint8_t Chip::setRegisterValue(const int registerNumber, const uint8_t value)
+{
+    if(registerNumber >= 0 && (registerNumber < sizeof(generalRegisters) / sizeof(generalRegisters[0])))
+    {
+        generalRegisters[registerNumber] = value;
+        return value;
+    }
+    
+    return -1;
 }
--- Chip.h
+++ Chip.h
@@ -12,13 +12,21 @@ class Chip
         // takes in a file/program name and loads into RAM
         Chip(std::string fileName);
         void start();
-        uint16_t fetch();
+
 
     private:
         uint16_t programCounter;
         void incrementProgramCounter();
         void decrementProgramCounter();
         void setProgramCounter(uint16_t address);
+        
+        uint16_t fetch();
+        uint16_t decode();
+
+        uint8_t generalRegisters[16];
+        uint8_t getRegisterValue(const int registerNumber) const;
+        uint8_t setRegisterValue(const int registerNumber, const uint8_t value);
+
         Font font;
         Ram ram;
         
