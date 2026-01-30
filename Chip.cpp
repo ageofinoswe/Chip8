1c2c93d27ad9a8e11cd5f5886335228f2844da64
Alexander <ageofinoswe@gmail.com>
2026-01-30 15:16:43 -0800

    added program counter functionality and fetch operation

--- Chip.cpp
+++ Chip.cpp
@@ -1,8 +1,9 @@
 #include "Chip.h"
 
 Chip::Chip(std::string fileName)
+    : programCounter{0x200}
 {   
-    int programStart = 0x200;
+    int programStart = programCounter;
 
     std::ifstream file(fileName, std::ios::binary | std::ios::in);
     char readByte;
@@ -14,4 +15,33 @@ Chip::Chip(std::string fileName)
         programStart += 0x1;
     }
     ram.memDump();
+}
+
+void Chip::start()
+{
+    return;
+}
+
+
+void Chip::incrementProgramCounter()
+{
+    programCounter += 0x2;
+}
+
+void Chip::decrementProgramCounter()
+{
+    programCounter -= 0x2;
+}
+
+void Chip::setProgramCounter(uint16_t address)
+{
+    programCounter = 0xfff & address;
+}
+
+uint16_t Chip::fetch()
+{
+    uint16_t msb = ram.readMem(programCounter) << 8;
+    uint16_t lsb = ram.readMem(programCounter + 0x1);
+    incrementProgramCounter();
+    return msb | lsb;
 }
--- Chip.h
+++ Chip.h
@@ -8,13 +8,20 @@
 class Chip
 {
     public:
+        /* constructors */
+        // takes in a file/program name and loads into RAM
         Chip(std::string fileName);
+        void start();
+        uint16_t fetch();
 
     private:
         uint16_t programCounter;
+        void incrementProgramCounter();
+        void decrementProgramCounter();
+        void setProgramCounter(uint16_t address);
         Font font;
         Ram ram;
-
+        
 };
 
 #endif
