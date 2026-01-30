4d0b2bf3ecdad23d8e0de0840ad09a0ba79aefcc
Alexander <ageofinoswe@gmail.com>
2026-01-29 17:09:48 -0800

    added sdl test window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include "Chip.h"
+#include <SDL3/SDL.h>
 
 int main()
 {
-    Chip chip;
+    SDL_Window *window;                    // Declare a pointer
+    bool done = false;
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    SDL_Delay(1000);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
 }
