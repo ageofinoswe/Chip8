6531690bc6777afaaf4020c48a6e388a2bec75b0
Alexander Fino <ageofinoswe@gmail.com>
2026-03-10 03:06:28 -0700

    working chip8

--- main.cpp
+++ main.cpp
@@ -3,19 +3,59 @@
 #include "Chip.h"
 
 using std::string;
+using std::stoi;
 
 int main(int argc, char *argv[])
 {
-    // provided chip8 file
-    string fileName = "IBM Logo.ch8";
+    // default file
+    string dir = "./ROMS/";
+    string defaultFileName = "IBM Logo";
+    string extension = ".ch8";
+    string fileName = dir + defaultFileName + extension;
 
     // user provided file
-    if(argc > 1)
-        fileName = argv[1];
-    
-    // begin chip8 emulation    
-    Chip chip(fileName);
-    chip.start(); 
-    
+    if(argc == 2)
+    {
+        fileName = dir + argv[1] + extension;
+        Chip chip(fileName);
+        chip.start();
+    }
+
+    // user provided file, opcode configs
+    else if(argc == 8)
+    {
+        fileName = dir + argv[1] + extension;
+        int shiftImpl = 1;
+        int jumpOffsetImpl = 1;
+        int storeLoadImpl = 1;
+
+        for(int i = 2 ; i < argc ; i++)
+        {
+            string arg = argv[i];
+
+            if(arg == "--shift")
+            {
+                shiftImpl = stoi(argv[++i]);
+            }
+            else if(arg == "--jump")
+            {
+                jumpOffsetImpl = stoi(argv[++i]);
+            }
+            else if(arg == "--memory")
+            {
+                storeLoadImpl = stoi(argv[++i]);
+            }
+        }
+        Chip chip(fileName, shiftImpl, jumpOffsetImpl, storeLoadImpl);
+        chip.start();
+    }
+
+    // run default file
+    else
+    {
+        Chip chip(fileName);
+        chip.start();
+    }
+
     return 0;
 }
